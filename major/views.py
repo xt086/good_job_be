83c6af91e3b6b9dd7fc789d7954fb85f0e300dd7
xtuan <you@example.com>
2024-05-12 16:37:11 +0700

    fix api major

--- major/views.py
+++ major/views.py
@@ -8,7 +8,7 @@ from .serializers import*
 from .models import Major
 from .serializers import MajorSerializer
 
-from .service import MajorService
+
 
 class APIMajor(viewsets.ModelViewSet):
     queryset = Major.objects.all()
@@ -20,7 +20,14 @@ class APIMajor(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
      
-        MajorService.create(request.data)
+        serializer_data = MajorSerializer(data=request.data)
+        if serializer_data.is_valid():
+            serializer_data.save()
+            status_code = status.HTTP_201_CREATED
+            return Response({"message": "Product Added Sucessfully", "status": status_code})
+        else:
+            status_code = status.HTTP_400_BAD_REQUEST
+            return Response({"message": "please fill the datails", "status": status_code})
 
     def destroy(self, request, *args, **kwargs):
         data = Major.objects.filter(id=kwargs['pk'])
